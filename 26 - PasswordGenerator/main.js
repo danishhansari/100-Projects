fe237ccd873f958f60c78dd51c18dc3172d543a3
Danish <dan71ish@gmail.com>
2022-12-07 16:10:55 +0530

    Password Generator finished

--- 26 - PasswordGenerator/main.js
+++ 26 - PasswordGenerator/main.js
@@ -1,5 +1,11 @@
 let text = document.querySelector('#text');
-document.querySelector('.btn').addEventListener('click',showPass);
+let pass1 = document.querySelector('.pass-1');
+let pass2 = document.querySelector('.pass-2');
+let btn = document.querySelector('.btn');
+btn.addEventListener('click', () => {
+    showPass()
+    showPass2()
+})
 function showPass(){
     let string = "abcdefghijklmnopqrstuvwxyz1234567890~!@#$%&*ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let password = "";
@@ -7,5 +13,14 @@ function showPass(){
         let ans = Math.floor(Math.random()*string.length);
         password += string.substring(ans,ans+1);   
     }
- text.value = password;
+    pass1.innerText = password;
+}
+function showPass2(){
+    let string = "abcdefghijklmnopqrstuvwxyz1234567890~!@#$%&*ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let password = "";
+    for(let i =0 ; i<=12;i++){
+        let ans = Math.floor(Math.random()*string.length);
+        password += string.substring(ans,ans+1);   
+    }
+    pass2.innerText = password;
 }
